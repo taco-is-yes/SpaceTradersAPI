a0b04d760b7212ebc2c0c97824698cd0d6a8e39e
Adrian Harting <76139114+taco-is-yes@users.noreply.github.com>
2022-11-14 14:04:21 +0000

    muney

--- main.py
+++ main.py
@@ -2,13 +2,12 @@ import requests
 from time import sleep
 import os
 import json
-import re
 
 def SellGoods(ShipId, GoodId, Amount):
 	requests.post('https://api.spacetraders.io/my/sell-orders', params={'shipId': ShipId, 'good': GoodId, 'quantity': Amount, 'token': os.getenv("TOKEN")})
 
 def BuyGoods(ShipId, GoodId, Amount):
-	requests.post('https://api.spacetraders.io/my/sell-orders', params={'shipId': ShipId, 'good': GoodId, 'quantity': Amount, 'token': os.getenv("TOKEN")})
+	requests.post('https://api.spacetraders.io/my/purchase-orders', params={'shipId': ShipId, 'good': GoodId, 'quantity': Amount, 'token': os.getenv("TOKEN")})
 
 def FlyTo(ShipId, Destination):
 	ResJson = requests.post('https://api.spacetraders.io/my/flight-plans', params={'shipId': ShipId, 'destination': Destination, "token": os.getenv("TOKEN")}).text
@@ -16,11 +15,15 @@ def FlyTo(ShipId, Destination):
 	try:
 		sleep(ResJson["flightPlan"]["timeRemainingInSeconds"])
 	except:
-		requests.post('https://api.spacetraders.io/my/purchase-orders', params={'shipId': ShipId, 'good': 'FUEL', 'quantity': int(re.search(r'\d+',ResJson['error']['message']).group()), 'token': os.getenv("TOKEN")})
-		FlyTo(ShipId, Destination)
+		if ResJson['error']['code'] != 3003:
+			requests.post('https://api.spacetraders.io/my/purchase-orders', params={'shipId': ShipId, 'good': 'FUEL', 'quantity': 1, 'token': os.getenv("TOKEN")})
+			FlyTo(ShipId, Destination)
+		else:
+			print(ResJson)
 
 while True:
 	BuyGoods("clag1cyu459697816s6koq9kcme", "FUEL", 2)
+	print("Bought")
 	FlyTo("clag1cyu459697816s6koq9kcme", "OE-PM-TR")
 	print("Traveled")
 	BuyGoods("clag1cyu459697816s6koq9kcme", "METALS", 99)
@@ -28,5 +31,5 @@ while True:
 	print("Bought")
 	FlyTo("clag1cyu459697816s6koq9kcme", "OE-PM")
 	print("Traveled")
-	SellGoods("clag1cyu459697816s6koq9kcme", "METALS", 98)
+	SellGoods("clag1cyu459697816s6koq9kcme", "METALS", 99)
 	print("Sold")
